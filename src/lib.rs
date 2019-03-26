9f64257066cf13396539c83fe5964dc7447b6db8
lukwol <wolanczyk.lukasz@gmail.com>
2019-03-26 22:40:54 +0100

    Working on PoC

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,58 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![no_std]
+// #![allow(dead_code)]
+
+#[derive(Debug)]
+struct Address(u8);
+
+impl From<Address> for u8 {
+    fn from(address: Address) -> Self {
+        address.0
     }
 }
+
+trait AddressPin {
+    fn bit() -> bool;
+}
+
+#[derive(Debug)]
+struct A0Pin;
+
+#[derive(Debug)]
+struct A1Pin;
+
+#[derive(Debug)]
+struct A2Pin;
+
+#[derive(Debug)]
+struct PinConfiguration {
+    a0_pin: Option<A0Pin>,
+    a1_pin: Option<A1Pin>,
+    a2_pin: Option<A2Pin>,
+}
+
+#[derive(Debug)]
+struct PinConfigurableAddress {
+    address: Address,
+    configuration: PinConfiguration,
+}
+
+trait Register {
+    fn address(&self) -> Address;
+}
+
+trait WriteRegister: Register {}
+
+trait WriteReadRegister: Register {}
+
+#[derive(Debug)]
+struct TemperatureRegister;
+
+impl Register for TemperatureRegister {
+    fn address(&self) -> Address {
+        Address(0x5)
+    }
+}
+
+impl WriteRegister for TemperatureRegister {}
+
+impl WriteReadRegister for TemperatureRegister {}
