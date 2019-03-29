9993310e47d2c6e8b0f22aed53a535ca15a67d6e
lukwol <wolanczyk.lukasz@gmail.com>
2019-03-29 11:17:15 +0100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@
 use crate::hal::blocking::i2c;
 use embedded_hal as hal;
 
-use core::marker::PhantomData;
-
 #[derive(Debug)]
 enum Error<E> {
     I2C(E),
@@ -20,55 +18,59 @@ impl From<Address> for u8 {
     }
 }
 
-struct Register<Size> {
-    address: Address,
-    _pd: PhantomData<Size>,
+trait Register {
+    fn address(&self) -> Address;
 }
 
-trait ReadRegister<'a, Size> {
-    fn read_register<I2C, Value, Err>(
-        &self,
-    ) -> &Fn(&mut I2C, Address, Address) -> Result<Value, Err>
+trait ReadRegister<'a, Size>: Register {
+    fn read<I2C, Err>(&self) -> &Fn(&mut I2C, Address, Address) -> Result<Size, Err>
     where
-        I2C: i2c::WriteRead<Error = Err>,
-        Value: From<Size>;
+        I2C: i2c::WriteRead<Error = Err>;
 }
 
-impl<'a> ReadRegister<'a, [u8; 2]> for Register<[u8; 2]> {
-    fn read_register<I2C, Value, Err>(
-        &self,
-    ) -> &Fn(&mut I2C, Address, Address) -> Result<Value, Err>
+struct Temperature(u16);
+
+impl From<[u8; 2]> for Temperature {
+    fn from(_: [u8; 2]) -> Self {
+        Temperature(0)
+    }
+}
+
+struct TemperatureRegister;
+
+impl Register for TemperatureRegister {
+    fn address(&self) -> Address {
+        Address(0)
+    }
+}
+
+impl<'a> ReadRegister<'a, [u8; 2]> for TemperatureRegister {
+    fn read<I2C, Err>(&self) -> &Fn(&mut I2C, Address, Address) -> Result<[u8; 2], Err>
     where
         I2C: i2c::WriteRead<Error = Err>,
-        Value: From<[u8; 2]>,
     {
         &|i2c, device_address, reg_address| {
             let mut buff = [0; 2];
             i2c.write_read(device_address.into(), &[reg_address.into()], &mut buff)?;
-            Ok(Value::from(buff))
+            Ok(buff)
         }
     }
 }
 
-//fn read_1_byte_register<Value, Err>(
-//    &mut self,
-//    register: &impl Read1BReg<Value>,
-//) -> Result<Value, Err>
-//    where
-//        I2C: i2c::WriteRead<Error = Err>,
-//        Value: From<[u8; 1]>,
-//{
-//    let mut buff = [0; 1];
-//    let address_ptr = self.address().into();
-//    self.i2c()
-//        .write_read(address_ptr, &[register.address().into()], &mut buff)?;
-//    Ok(Value::from(buff))
-//}
+#[derive(Debug)]
+struct I2cInterface<I2C> {
+    i2c: I2C,
+    address: Address,
+}
 
-//
-//trait WriteRegister<Payload> {
-//}
-
-//impl ReadRegister<[] for Register<[u8; 1]> {
-//
-//}
+impl<I2C> I2cInterface<I2C> {
+    fn read_register<'a, Size, Err>(
+        &mut self,
+        register: impl ReadRegister<'a, Size>,
+    ) -> Result<Size, Err>
+    where
+        I2C: i2c::WriteRead<Error = Err>,
+    {
+        register.read()(&mut self.i2c, self.address, register.address())
+    }
+}
