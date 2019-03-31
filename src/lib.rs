840120ef5d50454bfa7ecb4ddbe923b840b99625
lukwol <wolanczyk.lukasz@gmail.com>
2019-03-31 20:02:31 +0200

    Add upper, lower and critical temperature registers

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,9 @@ pub mod address;
 pub mod resolution;
 pub mod temperature;
 
+i2c_rw_reg!(UpperTemperatureRegister, addr: 0b0010, len: 2);
+i2c_rw_reg!(LowerTemperatureRegister, addr: 0b0011, len: 2);
+i2c_rw_reg!(CriticalTemperatureRegister, addr: 0b0100, len: 2);
 i2c_ro_reg!(AmbientTemperatureRegister, addr: 0b0101, len: 2);
 i2c_rw_reg!(ResolutionRegister, addr: 0b1000, len: 1);
 
--- src/temperature.rs
+++ src/temperature.rs
@@ -1,6 +1,9 @@
 use crate::hal::blocking::i2c;
-use crate::AmbientTemperatureRegister;
 use crate::MCP9808;
+use crate::{
+    AmbientTemperatureRegister, CriticalTemperatureRegister, LowerTemperatureRegister,
+    UpperTemperatureRegister,
+};
 
 const TEMPERATURE_SIGN_BIT: u8 = 0b1_0000;
 
@@ -88,251 +91,44 @@ where
     }
 }
 
-impl<I2C> MCP9808<I2C> {
-    pub fn read_ambient_temperature<Unit, Err>(&mut self) -> Result<Temperature<Unit>, Err>
-    where
-        I2C: i2c::WriteRead<Error = Err>,
-        Unit: From<[u8; 2]>,
-    {
-        self.i2c_interface.read_register(AmbientTemperatureRegister)
-    }
+macro_rules! read_temperature_register {
+    ($register: expr, $function_name: ident) => {
+        impl<I2C> MCP9808<I2C> {
+            pub fn $function_name<Unit, Err>(&mut self) -> Result<Temperature<Unit>, Err>
+            where
+                I2C: i2c::WriteRead<Error = Err>,
+                Unit: From<[u8; 2]>,
+            {
+                self.i2c_interface.read_register($register)
+            }
+        }
+    };
 }
 
-#[cfg(test)]
-mod conversion {
-    use crate::temperature::{Celsius, Millicelsius};
-
-    #[test]
-    fn celsius_to_millicelsius() {
-        assert_eq!(Celsius(25.367), Millicelsius(25_367).into());
-        assert_eq!(Celsius(-0.789), Millicelsius(-789).into());
-    }
-
-    mod temperature_to_raw {
-        use crate::temperature::{Celsius, Millicelsius};
-
-        #[test]
-        fn zero_millicelsius() {
-            assert_eq!([0b0000_0000, 0b0000_0000], {
-                let raw: [u8; 2] = Millicelsius(0).into();
-                raw
-            });
+macro_rules! write_temperature_register {
+    ($register: expr, $function_name: ident) => {
+        impl<I2C> MCP9808<I2C> {
+            pub fn $function_name<Unit, Err>(
+                &mut self,
+                temperature: Temperature<Unit>,
+            ) -> Result<(), Err>
+            where
+                I2C: i2c::Write<Error = Err>,
+                Unit: Into<[u8; 2]>,
+            {
+                self.i2c_interface.write_register($register, temperature)
+            }
         }
-
-        #[test]
-        fn zero_celsius() {
-            assert_eq!([0b0000_0000, 0b0000_0000], {
-                let raw: [u8; 2] = Celsius(0.0).into();
-                raw
-            });
-        }
-
-        #[test]
-        fn slightly_above_zero_millicelsius() {
-            assert_eq!([0b0000_0000, 0b0000_0001], {
-                let raw: [u8; 2] = Millicelsius(62).into();
-                raw
-            });
-            assert_eq!([0b0000_0000, 0b0000_0010], {
-                let raw: [u8; 2] = Millicelsius(125).into();
-                raw
-            });
-            assert_eq!([0b0000_0000, 0b0000_1000], {
-                let raw: [u8; 2] = Millicelsius(500).into();
-                raw
-            });
-            assert_eq!([0b0000_0000, 0b0000_1010], {
-                let raw: [u8; 2] = Millicelsius(625).into();
-                raw
-            });
-        }
-
-        #[test]
-        fn slightly_above_zero_celsius() {
-            assert_eq!([0b0000_0000, 0b0000_0001], {
-                let raw: [u8; 2] = Celsius(0.062).into();
-                raw
-            });
-            assert_eq!([0b0000_0000, 0b0000_0010], {
-                let raw: [u8; 2] = Celsius(0.125).into();
-                raw
-            });
-            assert_eq!([0b0000_0000, 0b0000_1000], {
-                let raw: [u8; 2] = Celsius(0.500).into();
-                raw
-            });
-            assert_eq!([0b0000_0000, 0b0000_1010], {
-                let raw: [u8; 2] = Celsius(0.625).into();
-                raw
-            });
-        }
-
-        #[test]
-        fn slightly_below_zero_millicelsius() {
-            assert_eq!([0b0001_1111, 0b1111_1111], {
-                let raw: [u8; 2] = Millicelsius(-63).into();
-                raw
-            });
-            assert_eq!([0b0001_1111, 0b1111_1110], {
-                let raw: [u8; 2] = Millicelsius(-125).into();
-                raw
-            });
-            assert_eq!([0b0001_1111, 0b1111_1100], {
-                let raw: [u8; 2] = Millicelsius(-250).into();
-                raw
-            });
-            assert_eq!([0b0001_1111, 0b1111_0111], {
-                let raw: [u8; 2] = Millicelsius(-563).into();
-                raw
-            });
-            assert_eq!([0b0001_1111, 0b1111_0101], {
-                let raw: [u8; 2] = Millicelsius(-688).into();
-                raw
-            });
-        }
-
-        #[test]
-        fn slightly_below_zero_celsius() {
-            assert_eq!([0b0001_1111, 0b1111_1111], {
-                let raw: [u8; 2] = Celsius(-0.063).into();
-                raw
-            });
-            assert_eq!([0b0001_1111, 0b1111_1110], {
-                let raw: [u8; 2] = Celsius(-0.125).into();
-                raw
-            });
-            assert_eq!([0b0001_1111, 0b1111_1100], {
-                let raw: [u8; 2] = Celsius(-0.250).into();
-                raw
-            });
-            assert_eq!([0b0001_1111, 0b1111_0111], {
-                let raw: [u8; 2] = Celsius(-0.563).into();
-                raw
-            });
-            assert_eq!([0b0001_1111, 0b1111_0101], {
-                let raw: [u8; 2] = Celsius(-0.688).into();
-                raw
-            });
-        }
-
-        #[test]
-        fn above_zero_millicelsius() {
-            assert_eq!([0b0000_00001, 0b1001_0100], {
-                let raw: [u8; 2] = Millicelsius(25250).into();
-                raw
-            });
-            assert_eq!([0b0000_00011, 0b1001_0110], {
-                let raw: [u8; 2] = Millicelsius(57375).into();
-                raw
-            });
-        }
-
-        #[test]
-        fn above_zero_celsius() {
-            assert_eq!([0b0000_00001, 0b1001_0100], {
-                let raw: [u8; 2] = Celsius(25.250).into();
-                raw
-            });
-            assert_eq!([0b0000_00011, 0b1001_0110], {
-                let raw: [u8; 2] = Celsius(57.375).into();
-                raw
-            });
-        }
-
-        #[test]
-        fn below_zero_millicelsius() {
-            assert_eq!([0b0000_11110, 0b0111_1111], {
-                let raw: [u8; 2] = Millicelsius(-24063).into();
-                raw
-            });
-            assert_eq!([0b0000_11100, 0b0111_1010], {
-                let raw: [u8; 2] = Millicelsius(-56375).into();
-                raw
-            });
-        }
-
-        #[test]
-        fn below_zero_celsius() {
-            assert_eq!([0b0000_11110, 0b0111_1111], {
-                let raw: [u8; 2] = Celsius(-24.063).into();
-                raw
-            });
-            assert_eq!([0b0000_11100, 0b0111_1010], {
-                let raw: [u8; 2] = Celsius(-56.375).into();
-                raw
-            });
-        }
-    }
-
-    mod raw_to_temperature {
-        use crate::temperature::{Celsius, Millicelsius};
-
-        #[test]
-        fn zero_millicelsius() {
-            assert_eq!(Millicelsius(0), [0b0000_0000, 0b0000_0000].into());
-        }
-
-        #[test]
-        fn zero_celsius() {
-            assert_eq!(Celsius(0.0), [0b0000_0000, 0b0000_0000].into());
-        }
-
-        #[test]
-        fn slightly_above_zero_millicelsius() {
-            assert_eq!(Millicelsius(62), [0b0000_0000, 0b0000_0001].into());
-            assert_eq!(Millicelsius(125), [0b0000_0000, 0b0000_0010].into());
-            assert_eq!(Millicelsius(500), [0b0000_0000, 0b0000_1000].into());
-            assert_eq!(Millicelsius(625), [0b0000_0000, 0b0000_1010].into());
-        }
-
-        #[test]
-        fn slightly_above_zero_celsius() {
-            assert_eq!(Celsius(0.062), [0b0000_0000, 0b0000_0001].into());
-            assert_eq!(Celsius(0.125), [0b0000_0000, 0b0000_0010].into());
-            assert_eq!(Celsius(0.500), [0b0000_0000, 0b0000_1000].into());
-            assert_eq!(Celsius(0.625), [0b0000_0000, 0b0000_1010].into());
-        }
-
-        #[test]
-        fn slightly_below_zero_millicelsius() {
-            assert_eq!(Millicelsius(-63), [0b0001_1111, 0b1111_1111].into());
-            assert_eq!(Millicelsius(-125), [0b0001_1111, 0b1111_1110].into());
-            assert_eq!(Millicelsius(-250), [0b0001_1111, 0b1111_1100].into());
-            assert_eq!(Millicelsius(-563), [0b0001_1111, 0b1111_0111].into());
-            assert_eq!(Millicelsius(-688), [0b0001_1111, 0b1111_0101].into());
-        }
-
-        #[test]
-        fn slightly_below_zero_celsius() {
-            assert_eq!(Celsius(-0.063), [0b0001_1111, 0b1111_1111].into());
-            assert_eq!(Celsius(-0.125), [0b0001_1111, 0b1111_1110].into());
-            assert_eq!(Celsius(-0.250), [0b0001_1111, 0b1111_1100].into());
-            assert_eq!(Celsius(-0.563), [0b0001_1111, 0b1111_0111].into());
-            assert_eq!(Celsius(-0.688), [0b0001_1111, 0b1111_0101].into());
-        }
-
-        #[test]
-        fn above_zero_millicelsius() {
-            assert_eq!(Millicelsius(25250), [0b0000_00001, 0b1001_0100].into());
-            assert_eq!(Millicelsius(57375), [0b0000_00011, 0b1001_0110].into());
-        }
-
-        #[test]
-        fn above_zero_celsius() {
-            assert_eq!(Celsius(25.250), [0b0000_00001, 0b1001_0100].into());
-            assert_eq!(Celsius(57.375), [0b0000_00011, 0b1001_0110].into());
-        }
-
-        #[test]
-        fn below_zero_millicelsius() {
-            assert_eq!(Millicelsius(-24063), [0b0000_11110, 0b0111_1111].into());
-            assert_eq!(Millicelsius(-56375), [0b0000_11100, 0b0111_1010].into());
-        }
-
-        #[test]
-        fn below_zero_celsius() {
-            assert_eq!(Celsius(-24.063), [0b0000_11110, 0b0111_1111].into());
-            assert_eq!(Celsius(-56.375), [0b0000_11100, 0b0111_1010].into());
-        }
-    }
+    };
 }
+
+read_temperature_register!(AmbientTemperatureRegister, read_ambient_temperature);
+
+read_temperature_register!(UpperTemperatureRegister, read_upper_temperature);
+write_temperature_register!(UpperTemperatureRegister, write_upper_temperature);
+
+read_temperature_register!(LowerTemperatureRegister, read_lower_temperature);
+write_temperature_register!(LowerTemperatureRegister, write_lower_temperature);
+
+read_temperature_register!(CriticalTemperatureRegister, read_critical_temperature);
+write_temperature_register!(CriticalTemperatureRegister, write_critical_temperature);
