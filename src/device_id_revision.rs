286d90982f7a6373961f816a8241ed71c8e3cdf2
lukwol <wolanczyk.lukasz@gmail.com>
2019-04-05 22:03:55 +0200

    Remove unnecessary DeviceIdRevision struct

--- src/device_id_revision.rs
+++ src/device_id_revision.rs
@@ -11,34 +11,19 @@ pub struct DeviceId(pub u8);
 #[derive(Debug, PartialEq, PartialOrd, Copy, Clone)]
 pub struct DeviceRevision(pub u8);
 
-#[derive(Debug, PartialEq, PartialOrd, Copy, Clone)]
-pub struct DeviceIdRevision {
-    pub device_id: DeviceId,
-    pub device_revision: DeviceRevision,
-}
-
 impl DeviceId {
     pub fn is_valid(self) -> bool {
         self.0 == VALID_DEVICE_ID
     }
 }
 
-impl From<Raw> for DeviceIdRevision {
-    fn from(raw: Raw) -> Self {
-        let (msb, lsb) = (raw[0], raw[1]);
-        DeviceIdRevision {
-            device_id: DeviceId(msb),
-            device_revision: DeviceRevision(lsb),
-        }
-    }
-}
-
 impl<I2C> MCP9808<I2C> {
-    pub fn read_device_information<Err>(&mut self) -> Result<DeviceIdRevision, Err>
+    pub fn read_device_information<Err>(&mut self) -> Result<(DeviceId, DeviceRevision), Err>
     where
         I2C: i2c::WriteRead<Error = Err>,
     {
         self.i2c_interface
             .read_register(&self.device_information_register)
+            .map(|raw: Raw| (DeviceId(raw[0]), DeviceRevision(raw[1])))
     }
 }
