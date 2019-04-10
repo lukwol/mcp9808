b62ba499cdd3484ac2b295591ee5c87bb7b800ed
lukwol <wolanczyk.lukasz@gmail.com>
2019-04-10 16:47:46 +0200

    Add manufacturer_id documentation.

--- src/manufacturer_id.rs
+++ src/manufacturer_id.rs
@@ -5,10 +5,15 @@ const VALID_MANUFACTURER_ID: u16 = 0x0054;
 
 type Raw = <ManufacturerIdRegister as Register>::Raw;
 
+/// `ManufacturerId` is used to identify the manufacturer of the
+/// device in order to perform manufacturer-specific
+/// operation
 #[derive(Debug, PartialEq, PartialOrd, Copy, Clone)]
 pub struct ManufacturerId(pub u16);
 
 impl ManufacturerId {
+
+    /// The Manufacturer ID for the MCP9808 is 0x0054 (hexadecimal)
     pub fn is_valid(self) -> bool {
         self.0 == VALID_MANUFACTURER_ID
     }
@@ -22,6 +27,8 @@ impl From<Raw> for ManufacturerId {
 }
 
 impl<I2C> MCP9808<I2C> {
+
+    /// Read `ManufacturerId` from `ManufacturerIdRegister`
     pub fn read_manufacturer_id<Err>(&mut self) -> Result<ManufacturerId, Err>
     where
         I2C: i2c::WriteRead<Error = Err>,
